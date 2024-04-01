6cd7c2766616f87ff1bb4b1663fb231471aa89c0
90sidorta <90sidort@gmail.com>
2024-04-01 17:44:25 +0200

    style main list

--- dw_blog_django/apps/blog/views.py
+++ dw_blog_django/apps/blog/views.py
@@ -7,3 +7,9 @@ class MainView(ListView):
     model = Post
     template_name = "blog/index.html"
     context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(episode_number__in=[1, 2, 3]).order_by(
+            "-episode_number"
+        )[:3]
