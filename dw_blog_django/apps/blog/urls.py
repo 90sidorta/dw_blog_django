ac491eb77473d5fea75b1f9af05324621bd78f85
90sidorta <90sidort@gmail.com>
2024-04-06 00:36:13 +0200

    add pagination:

--- dw_blog_django/apps/blog/urls.py
+++ dw_blog_django/apps/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from dw_blog_django.apps.blog.views import MainView
+from dw_blog_django.apps.blog.views import MainPostView, MainSeriesView
 
 urlpatterns = [
-    path("", MainView.as_view(), name="main"),
+    path("", MainPostView.as_view(), name="main"),
+    path("series/main_series", MainSeriesView.as_view(), name="main_series"),
 ]
--- dw_blog_django/apps/blog/views.py
+++ dw_blog_django/apps/blog/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from dw_blog_django.apps.blog.models import Post
 
 
-class MainView(ListView):
+class MainPostView(ListView):
     model = Post
     template_name = "blog/index.html"
     context_object_name = "posts"
@@ -21,4 +21,20 @@ class MainView(ListView):
                 86,
                 91,
             ]
-        ).order_by("-episode_number")[:10]
+        ).order_by("-episode_number")[:8]
+
+
+class MainSeriesView(ListView):
+    model = Post
+    template_name = "blog/pages/main_series.html"
+    content_object_name = "main_posts"
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(series="main").order_by("-episode_number")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["main_posts"] = context["object_list"]
+        return context
